6ddc59447f9c36f97e36e8bcb9d5d6612b0b9448
Leonardo Rodrigues Magalhães <93494734+scorplion159@users.noreply.github.com>
2022-06-11 15:53:37 -0300

    Inserção da lógica de captura de dados do formulario de cadastro de funcionarios e criação do objeto funcionario a ser salvo na implementação de localStorage feita nessa pagina pelo integrante Théo

--- devagro-frontend/src/app/components/admin/pages/funcionarios/funcionario-cadastro/funcionario-cadastro.component.ts
+++ devagro-frontend/src/app/components/admin/pages/funcionarios/funcionario-cadastro/funcionario-cadastro.component.ts
@@ -12,4 +12,65 @@ export class FuncionarioCadastroComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  onstructor() { }
+
+  funcionario_info:any = {
+    id: 0,
+    nome: "",
+    fazenda: "",
+    CPF: "",
+    telefone: "",
+    funcao_princila: "",
+    ativo: ""
+  }
+
+  mudar_nome(nome:string):void{
+    this.funcionario_info.nome = nome
+  }
+
+  mudar_fazenda(fazenda:string):void{
+    this.funcionario_info.fazenda = fazenda
+  }
+
+  mudar_CPF(CPF:string):void{
+    this.funcionario_info.CPF = CPF
+  }
+
+  mudar_telefone(telefone:string):void{
+    this.funcionario_info.telefone = telefone
+  }
+
+  mudar_funcao_princila(funcao_princila:string):void{
+    this.funcionario_info.funcao_princila = funcao_princila
+
+  }
+
+  mudar_ativo(ativo:boolean):void{
+    this.funcionario_info.ativo = ativo
+  }
+
+  apertar_cadastrar():void{
+    let id = Number(localStorage.getItem("quantidade_funcionarios"))
+    if(id == null){
+      this.funcionario_info.id = 0
+    }
+    else{
+      this.funcionario_info.id = id
+    }
+
+    let funcionario_info_json: string = JSON.stringify(this.funcionario_info)
+    localStorage.setItem("funcionario_" + String(id),funcionario_info_json)
+
+    localStorage.setItem("quantidade_funcionarios",String(id + 1))
+    
+    console.log(funcionario_info_json)
+  }
+
+  pegar_funcionario(id:Number):any{
+    return JSON.parse(String(localStorage.getItem("funcionario_" + String(id))))
+  }
+
+  
+
 }
