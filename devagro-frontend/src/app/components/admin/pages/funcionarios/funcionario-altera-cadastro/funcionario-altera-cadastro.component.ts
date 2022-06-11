73b3dd856638f18145fe219a627f83d5b3bc28c2
Leonardo Rodrigues Magalhães <93494734+scorplion159@users.noreply.github.com>
2022-06-11 15:20:18 -0300

    criação de um component responsavel pela pagina de alteração do cadastro de funcionario

---
+++ devagro-frontend/src/app/components/admin/pages/funcionarios/funcionario-altera-cadastro/funcionario-altera-cadastro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-altera-cadastro',
+  templateUrl: './funcionario-altera-cadastro.component.html',
+  styleUrls: ['./funcionario-altera-cadastro.component.css']
+})
+export class FuncionarioAlteraCadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
