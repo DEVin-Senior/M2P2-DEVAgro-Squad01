af6733eaba09355cbbdfcfdee7c488ae48f79b8c
Leonardo Rodrigues Magalhães <93494734+scorplion159@users.noreply.github.com>
2022-06-11 15:21:14 -0300

    criação de um componente responsável pelo cadastro de funcionaio

---
+++ devagro-frontend/src/app/components/admin/pages/funcionarios/funcionario-cadastro/funcionario-cadastro.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FuncionarioCadastroComponent } from './funcionario-cadastro.component';
+
+describe('FuncionarioCadastroComponent', () => {
+  let component: FuncionarioCadastroComponent;
+  let fixture: ComponentFixture<FuncionarioCadastroComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FuncionarioCadastroComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FuncionarioCadastroComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ devagro-frontend/src/app/components/admin/pages/funcionarios/funcionario-cadastro/funcionario-cadastro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-cadastro',
+  templateUrl: './funcionario-cadastro.component.html',
+  styleUrls: ['./funcionario-cadastro.component.css']
+})
+export class FuncionarioCadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
