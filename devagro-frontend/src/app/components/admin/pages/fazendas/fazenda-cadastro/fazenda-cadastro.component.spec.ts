bae8c37fac14d2be1c810e0760df7d485c478a5d
Leonardo Rodrigues Magalhães <93494734+scorplion159@users.noreply.github.com>
2022-06-12 11:53:25 -0300

    Atualização na lista que armazena o localStorage, se quando a função onInit for ativada o valor do localStorage for null, será aberta uma lista vazia

---
+++ devagro-frontend/src/app/components/admin/pages/fazendas/fazenda-cadastro/fazenda-cadastro.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FazendaCadastroComponent } from './fazenda-cadastro.component';
+
+describe('FazendaCadastroComponent', () => {
+  let component: FazendaCadastroComponent;
+  let fixture: ComponentFixture<FazendaCadastroComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FazendaCadastroComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FazendaCadastroComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
