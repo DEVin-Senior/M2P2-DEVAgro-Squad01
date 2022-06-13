eb36cea5053be459289e433c5327805fd3fc908f
Leonardo Rodrigues Magalhães <93494734+scorplion159@users.noreply.github.com>
2022-06-13 09:33:47 -0300

    Update funcionarios.component.ts

--- devagro-frontend/src/app/components/admin/pages/funcionarios/funcionarios.component.ts
+++ devagro-frontend/src/app/components/admin/pages/funcionarios/funcionarios.component.ts
@@ -13,7 +13,16 @@ export class FuncionariosComponent implements OnInit {
   localStorageViewObject : Array<Object> =[]
 
   ngOnInit(): void {
-    
+    console.log(this.localStorageView)
+    console.log(this.localStorageView)
+
+
+    this.localStorageView.forEach(element => {
+     console.log( JSON.parse(String(element)))
+      console.log(element)
+      console.log(element.isPrototypeOf)
+      
+    });
 
 
     
@@ -24,5 +33,9 @@ export class FuncionariosComponent implements OnInit {
 
   constroiLsitaDeObj(){}
 
+  
 
 }
+
+
+
