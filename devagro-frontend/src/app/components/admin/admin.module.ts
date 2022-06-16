ef335bed501733b4608996d41d3f350a82128286
tpdevinhouse <95538124+tpdevinhouse@users.noreply.github.com>
2022-06-15 21:48:12 -0300

    Atualização component grao

--- devagro-frontend/src/app/components/admin/admin.module.ts
+++ devagro-frontend/src/app/components/admin/admin.module.ts
@@ -12,8 +12,6 @@ import { GraosComponent } from './pages/graos/graos.component';
 import { SidenavComponent } from '../sidenav/sidenav.component';
 import { TemplateBodyComponent } from '../template-body/template-body.component';
 
-import { GraoCadastroComponent } from './pages/grao-cadastro/grao-cadastro.component';
-
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
@@ -22,7 +20,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { LayoutModule } from '@angular/cdk/layout';
 import { FazendaCadastroComponent } from './pages/fazendas/fazenda-cadastro/fazenda-cadastro.component';
-import { GraosCadastroComponent } from './pages/graos/graos-cadastro/graos-cadastro.component';
+import { GraoCadastroComponent } from './pages/grao-cadastro/grao-cadastro.component';
 import { FuncionarioCadastroComponent } from './pages/funcionarios/funcionario-cadastro/funcionario-cadastro.component';
 import { FuncionarioAlteraCadastroComponent } from './pages/funcionarios/funcionario-altera-cadastro/funcionario-altera-cadastro.component';
 
@@ -37,9 +35,9 @@ import { FuncionarioAlteraCadastroComponent } from './pages/funcionarios/funcion
     TemplateBodyComponent,
     DashboardComponent,
     FazendaCadastroComponent,
-    GraosCadastroComponent,
     FuncionarioCadastroComponent,
-    FuncionarioAlteraCadastroComponent
+    FuncionarioAlteraCadastroComponent,
+    GraoCadastroComponent
 
   ],
   imports: [
