dfcbdf57c2abca6a7d595f05310cfb60988fb1fd
all-an <all-an@hotmail.com>
2022-06-17 15:59:55 -0300

    Finalmente convertendo e retornando lista de grãos do localStorage

--- devagro-frontend/src/app/components/admin/pages/graos/graos.component.ts
+++ devagro-frontend/src/app/components/admin/pages/graos/graos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Object } from './grao';
 import { GRAOS } from './mock-grao';
 import { Router } from '@angular/router';
@@ -10,7 +10,10 @@ import { Router } from '@angular/router';
 })
 export class GraosComponent implements OnInit {
 
+  @Input() public titulo: string = "Grãos"
+
   graos = GRAOS;
+  graosObjetos :Array<Object>= [];
 
   selectedGrao?: Object;
   onSelect(grao: Object): void {
@@ -20,22 +23,33 @@ export class GraosComponent implements OnInit {
   key: any = 'listaGraos';
   myItem!: any | null;
   storeGrao() {
-    localStorage.setItem(this.key, JSON.stringify(this.graos));
-    this.graos = JSON.parse(String(localStorage.getItem("listaGraos")));
+    localStorage.setItem(this.key, JSON.stringify(this.graosObjetos));
+    this.graosObjetos = JSON.parse(String(localStorage.getItem("listaGraos")));
   }
   SpecificDelete(grao: Object) {
-    this.graos = this.graos.filter(h => h !== grao);
+    this.graos = this.graosObjetos.filter(h => h !== grao);
     localStorage.setItem(this.key, JSON.stringify(this.graos));
+    this.refreshPage();
   }
 
   deleteGraos() {
     localStorage.clear();
-    this.myItem = '';
-    this.graos = JSON.parse(String(localStorage.getItem("listaGraos")));
+    this.graosObjetos = JSON.parse(String(localStorage.getItem("listaGraos")));
   }
+
+  refreshPage() {
+    window.location.reload();
+  }
+   
 
   constructor(private router: Router) {
     this.graos = JSON.parse(String(localStorage.getItem("listaGraos")));
+    localStorage.setItem(this.key, JSON.stringify(this.graos));
+    if(this.graos != null){
+      for(let i = 0; i < this.graos.length; i++){
+        this.graosObjetos.push(JSON.parse(String(this.graos[i])))
+      }
+    }
   }
 
   btnClick = () => {
@@ -43,7 +57,7 @@ export class GraosComponent implements OnInit {
   };
 
   ngOnInit(): void {
-   
+
   }
 
 }
