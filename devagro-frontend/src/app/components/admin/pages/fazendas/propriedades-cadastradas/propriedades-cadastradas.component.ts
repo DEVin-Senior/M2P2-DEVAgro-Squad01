fcb8ea738ada368095b10868dc7f904e332196ef
Murillo Silva <95661887+Rumil0@users.noreply.github.com>
2022-06-17 19:35:54 -0300

    Atualização página de fazendas / dashboard (#36)
    * FazendasCard10
    
    Base front end fazendas card 10
    
    * Versão 1.1
    
    Com local storage
    
    * 1.2
    
    Visual aprimorado, nfor funcionando e localstorage funcionando
    
    * 1.3
    
    Com cadastro
    
    * Versão 1.4
    
    Ajustes de layout
    
    * V1.4.1
    
    Ajuste página de funcionários
    
    * 1.4.2 New component
    
    Adicionado component de propriedades
    
    * V1.5 Componente fazenda no dashboard
    
    Adicionado o componente fazenda ao dashboard e realizado ajustes estéticos no layout do mesmo.
    
    * 1.6 Organização tabela fazendas
    
    Adicionada a função de colocar a tabela em ordem crescente ou decrescente

--- devagro-frontend/src/app/components/admin/pages/fazendas/propriedades-cadastradas/propriedades-cadastradas.component.ts
+++ devagro-frontend/src/app/components/admin/pages/fazendas/propriedades-cadastradas/propriedades-cadastradas.component.ts
@@ -17,4 +17,64 @@ export class PropriedadesCadastradasComponent implements OnInit {
 
   }
 
+  nomeCrescente(): void{
+    sort(this.listaFazenda, 'nome', 'id', 'grao', 'ultima_colheita', 'localizacao')
+    console.log(this.listaFazenda)
+  }
+
+  nomeDecrescente(): void{
+    sort(this.listaFazenda, '-nome', 'id', 'grao', 'ultima_colheita', 'localizacao')
+    console.log(this.listaFazenda)
+  }
+
+  colheitaCrescente(): void{
+    sort(this.listaFazenda, 'ultima_colheita', 'id', 'nome', 'grao', 'localizacao')
+    console.log(this.listaFazenda)
+  }
+
+  colheitaDecrescente(): void{
+    sort(this.listaFazenda, '-ultima_colheita', 'id',  'nome', 'grao', 'localizacao')
+    console.log(this.listaFazenda)
+  }
+}
+
+// Treixo abaixo retirado da resposta apresentada pelo usuário Inigo no forum StackOverflow no seguinte link:
+// https://stackoverflow.com/a/68279093
+
+type sortArg<T> = keyof T | `-${string & keyof T}`
+
+export function byPropertiesOf<T extends object> (sortBy: Array<sortArg<T>>) {
+  function compareByProperty (arg: sortArg<T>) {
+      let key: keyof T
+      let sortOrder = 1
+      if (typeof arg === 'string' && arg.startsWith('-')) {
+          sortOrder = -1
+          // Typescript is not yet smart enough to infer that substring is keyof T
+          key = arg.substr(1) as keyof T
+      } else {
+          // Likewise it is not yet smart enough to infer that arg is not keyof T
+          key = arg as keyof T
+      }
+      return function (a: T, b: T) {
+          const result = a[key] < b[key] ? -1 : a[key] > b[key] ? 1 : 0
+
+          return result * sortOrder
+      }
+  }
+
+  return function (obj1: T, obj2: T) {
+      let i = 0
+      let result = 0
+      const numberOfProperties = sortBy?.length
+      while (result === 0 && i < numberOfProperties) {
+          result = compareByProperty(sortBy[i])(obj1, obj2)
+          i++
+      }
+
+      return result
+  }
+}
+
+export function sort<T extends object> (arr: T[], ...sortBy: Array<sortArg<T>>) {
+  arr.sort(byPropertiesOf<T>(sortBy))
 }
