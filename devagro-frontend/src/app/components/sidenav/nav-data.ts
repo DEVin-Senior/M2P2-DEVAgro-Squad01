9d520534a14f66fb4130afbd795e63b52e687417
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-06-19 09:59:51 -0300

    Atualização de icones personalizados

--- devagro-frontend/src/app/components/sidenav/nav-data.ts
+++ devagro-frontend/src/app/components/sidenav/nav-data.ts
@@ -1,22 +1,26 @@
 export const navbarData = [
   {
     routeLink: '/admin',
-    icon: "fal fa-home",
-    label: "Início"
+    //icon: "fal fa-home",
+    label: "Início",
+    pathImgHome: true
   },
   {
     routeLink: '/admin/funcionarios',
-    icon: "fal fa-users",
-    label: "Funcionários"
+    //icon: "fal fa-users",
+    label: "Funcionários",
+    pathImgFunc: true
   },
   {
     routeLink: '/admin/fazendas',
-    icon: "fal fa-university",
-    label: "Fazendas"
+    //icon: "fal fa-university",
+    label: "Fazendas",
+    pathImgArmazem: true
   },
   {
     routeLink: '/admin/graos',
-    icon: "fal fa-tree",
-    label: "Grãos"
+    //icon: "fal fa-tree",
+    label: "Grãos",
+    pathImgMilho: true
   },
 ];
